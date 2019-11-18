c9d4f7bc3fee692b361509cd2b6c7e601696bd35
Шмалюк Дмитро <shmalyk9000@gmail.com>
2019-11-19 00:29:58 +0200

    added navigation menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './style.css';
 
+const navigationElements = ["Головна", "Тварини", "Локації"];
+
 export default function Header(){
     return(
         <header>
             <h1><span>The Online Zoo</span></h1>
+            <nav>
+                <ul>
+                    {navigationElements.map(el => <li><span>{el}</span></li>)}
+                </ul>
+            </nav>
         </header>
     )
 }
