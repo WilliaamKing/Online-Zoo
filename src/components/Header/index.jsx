481e5ebe9c7f004a99be99aff02d1eb178fa70af
Шмалюк Дмитро <shmalyk9000@gmail.com>
2019-11-18 23:57:05 +0200

    added color and name to header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function Header(){
+    return(
+        <header>
+            <h1><span>The Online Zoo</span></h1>
+        </header>
+    )
+}
