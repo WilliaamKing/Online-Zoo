2eed9613da342e7e94945ef848a88e34f498099a
Шмалюк Дмитро <shmalyk9000@gmail.com>
2019-11-19 01:30:57 +0200

    added three route into application and created components: Home, Animals, Places

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {Fragment}from 'react';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './components/Header/index';
+import Home from './components/Home/index';
+import Animals from './components/Animals/index';
+import Places from './components/Places/index';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-       <Header />
-    </div>
+    <Fragment>
+       <Router>
+          <div className="app">
+              <Header />
+
+              <div className = "content">
+                  <Switch>
+                    <Route exact path = '/' component = {Home}/>
+                    <Route path = '/animals' component = {Animals}/>
+                    <Route path = '/places' component = {Places}/>
+                  </Switch>
+              </div>
+          </div>
+       </Router>
+    </Fragment>
+    
   );
 }
 
---
+++ src/components/Animals/index.jsx
@@ -0,0 +1,9 @@
+import React, {Fragment} from 'react';
+
+export default function Animals(){
+    return (
+        <Fragment>
+            Animals
+        </Fragment>
+    )
+}
