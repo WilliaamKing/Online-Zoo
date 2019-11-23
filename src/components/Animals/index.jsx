5d8189a1539a3a5c1b28109c891f7a99dd9eebd1
Dmytro Shmaliuk <shmalyk9000@gmail.com>
2019-11-23 23:12:11 +0200

    update (Animals/index.js, Animals/style.css): add tool bar section and it's style

--- src/components/Animals/index.jsx
+++ src/components/Animals/index.jsx
@@ -1,5 +1,7 @@
 import React, {Fragment} from 'react';
+import { Button } from 'antd';
 import Animal from '../Animal/index';
+import 'antd/dist/antd.css';
 import './style.css';
 
 const store = [{name: "Горила рівнинна"},{name: "Казуар шоломоносний"},{name: "Сом червонохвостовий"},
@@ -8,6 +10,12 @@ export default function Animals(){
     return (
         <Fragment>
             {store.map(el => <Animal name = {el.name}  image = "https://zoo.kiev.ua/upload/iblock/507/5071ade6a92f09e52cd626ba5fd91f7d.JPG"/>)}
+
+            <article className = "tool-bar">
+                <Button type="primary" size = "large">Додати</Button>
+                <Button type="danger" size = "large">Видалити</Button>
+                <Button size = "large">Змінити</Button>
+            </article>
         </Fragment>
     )
 }
