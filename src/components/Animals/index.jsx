eec84d756b51d398e36a1a9b3fe95f07ac8ac6a6
Dmytro Shmaliuk <shmalyk9000@gmail.com>
2019-11-23 22:02:33 +0200

    add (Animals/index.js) animals card

--- src/components/Animals/index.jsx
+++ src/components/Animals/index.jsx
@@ -1,9 +1,13 @@
 import React, {Fragment} from 'react';
+import Animal from '../Animal/index';
+import './style.css';
 
+const store = [{name: "Горила рівнинна"},{name: "Казуар шоломоносний"},{name: "Сом червонохвостовий"},
+    {name: "Ропуха колорадська"},{name: "Скорпіон імператорський"},{name: "Анаконда парагвайська"}];
 export default function Animals(){
     return (
         <Fragment>
-            Animals
+            {store.map(el => <Animal name = {el.name}  image = "https://zoo.kiev.ua/upload/iblock/507/5071ade6a92f09e52cd626ba5fd91f7d.JPG"/>)}
         </Fragment>
     )
 }
