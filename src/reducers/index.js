f75cf5fc57bc7ff2dbd335e3548645033b212a71
Dmytro Shmaliuk <shmalyk9000@gmail.com>
2019-11-24 16:40:00 +0200

    refactor (reducers/index.jsx): combine animals and locations reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux';
 import animals from './animals';
+import locations from './locations'
 
-const rootReducer = combineReducers({animals});
+const rootReducer = combineReducers({animals,locations});
 export default rootReducer;
