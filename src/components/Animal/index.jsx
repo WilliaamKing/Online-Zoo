25a3903cf3ac8af3c9767e44fac6ad3f28199a63
Dmytro Shmaliuk <shmalyk9000@gmail.com>
2019-11-24 12:48:49 +0200

    add (Animal/index.jsx): additional information section

--- src/components/Animal/index.jsx
+++ src/components/Animal/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import './style.css';
 
-export default function Animal({name, image}){
+export default function Animal({info}){
     return (
-        <div className = "animal-card" style  = {{background: `url(${image})`}}>
+        <div className = "animal-card" style  = {{background: `url(${info.photo})`}}>
             <section>
-                <p>{name}</p>
+                <p>{info.species}</p>
+            </section>
+
+            <section id = "additionalInfo">
+                <h2>Біологічна класифікація</h2>
+                <p>Тип: {info.typeName}</p>
+                <p>Клас: {info.className}</p>
+                <p>Ряд: {info.series}</p>
+                <p>Родина: {info.family}</p>
+                <p>Рід: {info.genus}</p>
+                <p>Вид: {info.species}</p>
             </section>
         </div>
     );
